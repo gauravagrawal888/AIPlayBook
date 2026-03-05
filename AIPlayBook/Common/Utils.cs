ceafe9f260de1f4e40521657fd1163f16c3abddb
Agrawal, Gaurav <gaurav.f.agrawal@accenture.com>
2026-03-05 14:26:30 +0530

    Initial commit for Draw.IO MCP Agent

---
+++ AIPlayBook/Common/Utils.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+
+namespace Common
+{
+    [PublicAPI]
+    public static class Utils
+    {
+        public static void WriteLineDarkGray(string text)
+        {
+            WriteLine(text, ConsoleColor.DarkGray);
+        }
+
+        public static void WriteLine(string text, ConsoleColor color)
+        {
+            ConsoleColor orgColor = Console.ForegroundColor;
+            try
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(text);
+            }
+            finally
+            {
+                Console.ForegroundColor = orgColor;
+            }
+        }
+
+        public static void Separator()
+        {
+            Console.WriteLine();
+            WriteLine("".PadLeft(Console.WindowWidth, '-'), ConsoleColor.Gray);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AIPlayBook/DrawIOMcpServer/DrawIoMCPAgent.cs
@@ -0,0 +1,52 @@
+﻿using Azure.AI.OpenAI;
+using Common;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using ModelContextProtocol.Client;
+using OpenAI.Chat;
+using System.ClientModel;
+
+namespace DrawIOMcpServer
+{
+    public class DrawIoMCPAgent
+    {
+        public static async Task RunAsync(Configuration configuration)
+        {
+            AzureOpenAIClient client = new(new Uri(configuration.AzureOpenAIEndpoint),
+                new ApiKeyCredential(configuration.AzureOpenAIApiKey));
+
+            await using var drawIoMcpClient = await McpClient.CreateAsync(
+              new StdioClientTransport(new()
+              {
+                  Name = "Draw.IO MCP Server",
+                  Command = "npx",
+                  Arguments = ["-y", "drawio-mcp-server", "--editor", "--http-port", "4000"]
+              }));
+
+            var mcpTools = await drawIoMcpClient.ListToolsAsync().ConfigureAwait(false);
+
+            AIAgent agent = client
+                .GetChatClient("gpt-4o")
+                .AsAIAgent(
+                    instructions: "You are a Draw.IO MCP Server which can draw diagrams based on user prompts. Always apply colors to the shapes. ",
+                    tools: [.. mcpTools.Cast<AITool>()]
+                )
+                .AsBuilder()
+                .Build();
+
+            while (true)
+            {
+                Console.Write("> ");
+                string? input = Console.ReadLine();
+                if(input is null)
+                {
+                    break;
+                }
+                AgentResponse response = await agent.RunAsync(input);
+
+                Console.WriteLine(response);
+                Utils.Separator();
+            }
+        }
+    }
+}
